ced80a76ff71899b0a6dd7ec6c2456d9cc5f5062
RA1911003020517 <mr4131@srmist.edu.in>
2022-10-12 16:08:33 +0530

    completed pages , components , navbar , routes , layouts setup

---
+++ client/src/layouts/Homepage.layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+//import Navbar from "../components/Navbar";
+//import FoodTab from "../components/Foodtab";
+
+const HomepageLayout = ( Component ) => ( { ...props } ) => {
+  return (
+    <>
+      
+      {/*<Navbar />*/ }
+      
+      {/*<FoodTab/>*/ }
+
+      <div className='container mx-auto px-4 lg:px-20' >
+        <component {...props}/>
+      </div>
+    </>
+  )
+}
+
+export default HomepageLayout;
---
+++ client/src/pages/Home.page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//layout
+import HomepageLayout from '../layouts/Homepage.layout';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default HomepageLayout(Home);
