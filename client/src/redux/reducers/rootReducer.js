2b83ab5df1434de9b72ebf0c5b359976d6c1f9bb
RA1911003020517 <mr4131@srmist.edu.in>
2022-10-31 22:54:29 +0530

    added reviews reducer

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -5,12 +5,14 @@ import auth from "./auth/auth.reducer";
 import user from "./user/user.reducer";
 import restaurant from "./restaurant/restaurant.reducer";
 import image from "./image/image.reducer";
+import review from "./review/review.reducer";
 
 const rootReducer = combineReducers({
   auth,
   user,
   restaurant,
   image,
+  review,
 });
 
 export default rootReducer;
